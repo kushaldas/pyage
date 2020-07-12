6ad60ecea40f49828fe2fa8941ba7eadf5a030c5
Kushal Das <mail@kushaldas.in>
2020-07-12 23:21:04 +0530

    Adds encrypt_file and decrypt_file functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use std::io;
 use std::io::{Read, Write};
+use std::fs::File;
 
 use age;
+use age::cli_common::file_io;
 use pyo3::exceptions::*;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
@@ -22,7 +25,7 @@ fn create_newkey() -> PyResult<(String, String)> {
 /// keys: a list of public keys as str
 #[pyfunction]
 #[text_signature = "(data, keys)"]
-fn encrypt_bytes(py: Python, data: Vec<u8>, reps: Vec<String>) -> PyResult<PyObject>{
+fn encrypt_bytes(py: Python, data: Vec<u8>, reps: Vec<String>) -> PyResult<PyObject> {
     let mut recipients: Vec<age::keys::RecipientKey> = Vec::new();
 
     for rep in &reps {
@@ -33,7 +36,9 @@ fn encrypt_bytes(py: Python, data: Vec<u8>, reps: Vec<String>) -> PyResult<PyObj
     let encryptor = age::Encryptor::with_recipients(recipients);
 
     let mut encrypted = vec![];
-    let mut writer = encryptor.wrap_output(&mut encrypted, age::Format::Binary).unwrap();
+    let mut writer = encryptor
+        .wrap_output(&mut encrypted, age::Format::Binary)
+        .unwrap();
     writer.write_all(&data[..]).unwrap();
     writer.finish().unwrap();
 
@@ -43,7 +48,7 @@ fn encrypt_bytes(py: Python, data: Vec<u8>, reps: Vec<String>) -> PyResult<PyObj
 /// This function decrypts a given bytes using the secret key as str.
 #[pyfunction]
 #[text_signature = "(data, secret)"]
-fn decrypt_bytes(py: Python, data: Vec<u8>, secret: String) -> PyResult<PyObject>{
+fn decrypt_bytes(py: Python, data: Vec<u8>, secret: String) -> PyResult<PyObject> {
     let decryptor = match age::Decryptor::new(&data[..]).unwrap() {
         age::Decryptor::Recipients(d) => d,
         _ => unreachable!(),
@@ -57,11 +62,64 @@ fn decrypt_bytes(py: Python, data: Vec<u8>, secret: String) -> PyResult<PyObject
     Ok(PyBytes::new(py, &decrypted).into())
 }
 
+/// This function encryptes the given file using a list of public keys (as str),
+/// and writes the encrypted data to the outputfile.
+/// filename: Input filename
+/// outputfile: Output encrypted filename
+/// keys: The list of public keys as str.
+#[pyfunction]
+#[text_signature = "(filename, outputfile, keys)"]
+fn encrypt_file(filename: String, outputfile: String, keys: Vec<String>) -> PyResult<bool> {
+    let mut recipients: Vec<age::keys::RecipientKey> = Vec::new();
+
+    for rep in &keys {
+        let key = rep.parse().unwrap();
+        recipients.push(key);
+    }
+
+    //let format = file_io::OutputFormat::Binary;
+    let encryptor = age::Encryptor::with_recipients(recipients);
+    //let mut input = age::cli_common::file_io::InputReader::new(Some(filename)).unwrap();
+    let mut input = File::open(filename).unwrap();
+    //let output = file_io::OutputWriter::new(Some(outputfile), format, 0o666).unwrap();
+    let output = File::create(outputfile).unwrap();
+    let mut writer = encryptor.wrap_output(output, age::Format::Binary).unwrap();
+
+    io::copy(&mut input, &mut writer).unwrap();
+    writer.finish().unwrap();
+    Ok(true)
+}
+
+/// This function encryptes the given file using a list of secret keys (as str),
+/// and writes decrypted data to the outputfile.
+/// filename: Input filename
+/// outputfile: Output encrypted filename
+/// secret: The secret key as str
+#[pyfunction]
+#[text_signature = "(filename, outputfile, secret)"]
+fn decrypt_file(filename: String, outputfile: String, secret: String) -> PyResult<bool> {
+
+    match age::Decryptor::new(file_io::InputReader::new(Some(filename)).unwrap()).unwrap() {
+        age::Decryptor::Recipients(decryptor) => {
+            let mut output = File::create(outputfile).unwrap();
+            let key = age::keys::Identity::from_buffer(secret.as_bytes()).unwrap();
+            let mut inc = decryptor.decrypt(&key).unwrap();
+
+            io::copy(&mut inc, &mut output).unwrap();
+
+        },
+        _ => (),
+    }
+    Ok(true)
+}
+
 #[pymodule]
 /// A Python module implemented in Rust.
 fn pyage(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(create_newkey))?;
     m.add_wrapped(wrap_pyfunction!(encrypt_bytes))?;
     m.add_wrapped(wrap_pyfunction!(decrypt_bytes))?;
+    m.add_wrapped(wrap_pyfunction!(encrypt_file))?;
+    m.add_wrapped(wrap_pyfunction!(decrypt_file))?;
     Ok(())
 }
--- tests/test_encrypt_decrypt.py
+++ tests/test_encrypt_decrypt.py
@@ -17,3 +17,13 @@ def test_encrypt_decrypt_bytes():
 
     assert text == decrypted_in_bytes.decode("utf-8")
 
+def test_encrypt_decrypt_files():
+    with open("tests/files/public.txt") as f:
+        public = f.read()
+    print(public)
+    with open("tests/files/secret.txt") as f:
+        secret = f.read()
+
+    assert pyage.encrypt_file("/etc/hosts", "/tmp/ehost.age", [public,])
+    assert pyage.decrypt_file("/tmp/ehost.age", "/tmp/hosts.txt", secret)
+
